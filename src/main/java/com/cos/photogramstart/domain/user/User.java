396fe8644e90655f1b1f10e4706c82e07c70d08e
emperor-juwon <fwangjuwon@gmail.com>
2022-06-03 15:46:32 +0900

    User 모델링, DB insert확인

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,46 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor //빈 생성자
+@AllArgsConstructor //전체 생성자
+@Data //getter,setter
+@Entity //디비에 테이블을 생성시켜준다.
+public class User {
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //번호증가전략: 데이터베이스를 따라가겠다. 
+    @Id
+    private int id;
+    
+    private String username;
+    private String password;
+
+    private String name;
+    private String website; //웹사이트
+    private String bio; //자기소개
+    private String email;
+    private String phone;
+    private String gender;
+    
+    private String profileImgUrl; //사진
+    private String role; //권한
+
+    private LocalDateTime createDate;
+
+    @PrePersist //디비에 insert되기 전에 실행된다. 
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,11 +1,30 @@
 package com.cos.photogramstart.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller //1. ioc등록 2. file리턴 컨트롤러 
 public class AuthController {
+    
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+  
+    //di
+    private final AuthService authService;
+
+    //public AuthController(AuthService authService){
+    // this.authService=authService;
+    // }
     
     @GetMapping("/auth/signin")
     public String signinForm() {
@@ -19,8 +38,17 @@ public class AuthController {
 
     //회원가입 버튼 클릭 -> /auth/signup -> 리턴하면 /auth/signin
      @PostMapping("/auth/signup")
-     public String signup() {
-        System.out.println("signup 실행됨?");
-        return "auth/signin";
+     public String signup(SignupDto signupDto) {
+
+         log.info(signupDto.toString());
+         
+         //user에다가 singnupdto를 담기
+         User user = signupDto.toEntity();
+         log.info(user.toString());
+    
+         User userEntity = authService.회원가입(user);
+            System.out.println(userEntity); 
+        
+         return "auth/signin";
     }
 }
