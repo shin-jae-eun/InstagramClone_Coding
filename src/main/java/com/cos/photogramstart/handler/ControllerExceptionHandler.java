877232c05dc3f1ed01e6d35f8ca4bbdf792e92c0
emperor-juwon <fwangjuwon@gmail.com>
2022-06-07 15:22:39 +0900

    회원가입 완료 - 공통응답 script생성

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 
 @RestController
 @ControllerAdvice //모든 exception을 낚아챈다. 
 public class ControllerExceptionHandler {
     
-    //runtime 익셉션이 발동하는 모든 익셉션을 가로챈다. 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String,String> validationException(CustomValidationException e) {
-        return e.getErrorMap();
+    public String validationException(CustomValidationException e) {
+        
+        return Script.back(e.getErrorMap().toString());
     }
 }
