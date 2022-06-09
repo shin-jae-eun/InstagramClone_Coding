dcb33baf894f565dd8753a7818eaa811dcf9c60b
emperor-juwon <fwangjuwon@gmail.com>
2022-06-09 19:26:44 +0900

    이미지 업로드기능 완료

---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,42 @@
+package com.cos.photogramstart.service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "-" + imageUploadDto.getFile().getOriginalFilename();
+        System.out.println(imageFileName);
+
+        // db저장
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName); // 실제 어디 저장할건지 여기 넣어야한다.
+
+        // 통신 i/o 예외가 발생할 수 있다.
+        try {
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,23 +1,43 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.service.ImageService;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class ImageController {
-    
-    @GetMapping({"/","/image/story"})
+
+    private final ImageService imageService;
+
+    @GetMapping({ "/", "/image/story" })
     public String story() {
         return "image/story";
     }
 
-        @GetMapping({"/image/popular"})
-        public String popular() {
-            return "image/popular";
-        }
-    
-        @GetMapping({"/image/upload"})
+    @GetMapping({ "/image/popular" })
+    public String popular() {
+        return "image/popular";
+    }
+
+    @GetMapping({ "/image/upload" })
     public String upload() {
         return "image/upload";
+    }
+
+    @PostMapping("/image")
+    public String imageUpload(ImageUploadDto imageUploadDto,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        // service호출
+        imageService.사진업로드(imageUploadDto, principalDetails);
+
+        return "redirect:/user/" + principalDetails.getUser().getId();
     }
 }
