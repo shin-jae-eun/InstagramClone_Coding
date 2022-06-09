69173a85fd0643db3e87e05405850cb9e9bc50c3
emperor-juwon <fwangjuwon@gmail.com>
2022-06-09 19:56:39 +0900

    사진 업로드 db 저장 완료

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 
@@ -37,6 +38,12 @@ public class ImageService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // db저장
+        Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser()); // uuid
+        Image imageEntity = imageRepository.save(image);
+
+        System.out.println(imageEntity);
     }
 
 }
